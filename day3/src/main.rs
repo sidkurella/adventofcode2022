ec7e3247c862276038f41d68dcb0cb40d1017666
Siddharth Kurella <siddharthkurella@gmail.com>
2022-12-16 20:06:38 -0500

    Solve day 3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,8 @@
 use std::collections::HashSet;
 use std::io::stdin;
 
+const GROUP_SIZE: usize = 3;
+
 fn get_priority(c: char) -> Option<usize> {
     if c.is_ascii_lowercase() {
         return Some((c as usize) - ('a' as usize) + 1);
@@ -13,21 +15,25 @@ fn get_priority(c: char) -> Option<usize> {
 
 fn main() {
     let mut sum: usize = 0;
-    for line in stdin().lines() {
-        let raw_line = line.unwrap();
-        let l = raw_line.trim();
-        let partition = l.len() / 2;
-        let (compartment1, compartment2) = (&l[..partition], &l[partition..]);
-        let mut items = HashSet::new();
-        for c in compartment1.chars() {
-            items.insert(c);
+    let lines: Vec<String> = stdin().lines().map(|x| x.unwrap()).collect();
+    for groups in lines.as_slice().chunks_exact(GROUP_SIZE) {
+        let mut rucksacks = Vec::new();
+        for group in groups {
+            let l = group.trim();
+
+            let items: HashSet<char> = l.chars().collect();
+            rucksacks.push(items);
         }
-        for c in compartment2.chars() {
-            if items.contains(&c) {
-                sum += get_priority(c).unwrap();
-                break;
-            }
-        }
+
+        let intersection = rucksacks
+            .iter()
+            .cloned()
+            .reduce(|a, x| x.intersection(&a).copied().collect())
+            .unwrap();
+
+        let common = intersection.iter().next().unwrap();
+
+        sum += get_priority(*common).unwrap();
     }
 
     println!("{}", sum);
